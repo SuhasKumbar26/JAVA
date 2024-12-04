48bdbf5b18fbf69355cb313a47f1e4c41678133c
Suhas Kumbar <suhaskumbar1043@gmail.com>
2024-12-04 12:45:32 +0530

    Basic Programs

---
+++ Lecture1/src/PyramidsOutput.java
@@ -0,0 +1,17 @@
+public class PyramidsOutput {
+    public static void main(String[] args) {
+        System.out.println("Right half Pyramid");
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *\n");
+        System.out.println("------------------");
+        System.out.println("Reverse Right half Pyramid");
+        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*\n");
+        System.out.println("--------------------------");
+        System.out.println("Reverse Right half Pyramid");
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+        System.out.println("--------------------------");
+    }
+}
